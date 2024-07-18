2291368b376e0fcb8ec17208fed2b116c44692f6
trishamasbate <trisha.masbate@gmail.com>
2024-07-18 12:10:59 -0600

    Added the code to the database.js file.

---
+++ config/database.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false
+});
+
+module.exports = sequelize;
