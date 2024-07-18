27601d18c782b33ef99e5319e8e56805eb39a11a
trishamasbate <trisha.masbate@gmail.com>
2024-07-18 12:09:28 -0600

    Added the code for the server.js file.

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const session = require('express-session');
+const { Server } = require('socket.io');
+const http = require('http');
+const path = require('path');
+const { sequelize } = require('./models');
+const authRoutes = require('./routes/authRoutes');
+const bookRoutes = require('./routes/bookRoutes');
+const borrowRoutes = require('./routes/borrowRoutes');
+
+require('dotenv').config();
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.engine('handlebars', require('express-handlebars')());
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use('/auth', authRoutes);
+app.use('/books', bookRoutes);
+app.use('/borrow', borrowRoutes);
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+sequelize.sync().then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
+});
