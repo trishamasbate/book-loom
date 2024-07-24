05fc8ff5cc5d72816ee63e3cb3a32060edda4ed6
Shu Yang <shuyang@Thomass-MacBook-Pro.local>
2024-07-23 19:45:54 -0600

    connect db

--- config/database.js
+++ config/database.js
@@ -6,10 +6,9 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     dialect: 'postgres',
-    logging: false
+    logging: false,
+    host: 'localhost'
   }
 );
 
--- models/user.js
+++ models/user.js
@@ -1,21 +1,47 @@
-module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-      },
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false
-      }
-    });
+// module.exports = (sequelize, DataTypes) => {
+//     const User = sequelize.define('User', {
+//       id: {
+//         type: DataTypes.INTEGER,
+//         autoIncrement: true,
+//         primaryKey: true
+//       },
+//       username: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         unique: true
+//       },
+//       password: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//       }
+//     });
   
-    return User;
-  };
+//     return User;
+//   };
   
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+class User extends Model {}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  sequelize,
+  modelName: 'User',
+});
+
+module.exports = User;
